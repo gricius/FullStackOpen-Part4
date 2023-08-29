618e4a7840bf08a64aa8b9596bf6c5dabe0ec068
gricius <gricius@gmail.com>
2023-08-29 22:43:49 +0200

    add lint and jest

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const config = require('./utils/config')
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const blogsRouter = require('./controllers/blog');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blog')
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-console.log('connecting to', config.MONGODB_URI);
+console.log('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   }
   )
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
+    console.log('error connecting to MongoDB:', error.message)
   }
-  );
+  )
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', blogsRouter);
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-module.exports = app;
+module.exports = app
